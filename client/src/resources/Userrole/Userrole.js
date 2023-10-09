67b5abf0ffa1c11c797557b7e1437646485ccb89
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2023-10-09 22:02:54 +0500

    starting working on user role page

--- client/src/resources/Userrole/Userrole.js
+++ client/src/resources/Userrole/Userrole.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "../../css/reusable.css";
+import "../../css/Userrole/Userrole.css";
 
 const Userrole = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <div className="main">
+        <div className="userrole-main">
+          <div className="userrole-header flex flex-row align-center justify-center">
+            <div className="userrole-header-logo-section flex flex-row align-center justify-start">
+              <div className="userrole-logo"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Userrole
+export default Userrole;
