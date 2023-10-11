c10a9ea620eda985f90c029766694fb0a72fcd37
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2023-10-12 00:34:01 +0500

    UserLogin page 80% completed

--- client/src/resources/Userlogin/Userlogin.js
+++ client/src/resources/Userlogin/Userlogin.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/reusable.css";
 import "../../css/Userlogin/Userlogin.css";
+import { useNavigate } from "react-router-dom";
+
 
 const Userlogin = () => {
   const [selectedRole, setSelectedRole] = useState("");
@@ -8,6 +10,10 @@ const Userlogin = () => {
   const handleRadioChange = (event) => {
     setSelectedRole(event.target.value);
   };
+  const navigate = useNavigate();
+  useEffect(() => {
+    document.title = "Investify | UserLogin";
+  });
   return (
     <>
       <div className="main">
@@ -32,34 +38,34 @@ const Userlogin = () => {
                       type="radio"
                       name="role"
                       value="investor"
-                      className="userlogin-checkbox"
+                      className="userlogin-radio"
                       checked={selectedRole === "investor"}
                       onChange={handleRadioChange}
                     />
-                    <h5>Investor</h5>
+                    <h5 className="userlogin-inputfields-label">Investor</h5>
                   </div>
                   <div className="userlogin-loginarea-left-loginform-inputarearadio flex flex-row">
                     <input
                       type="radio"
                       name="role"
                       value="investee"
-                      className="userlogin-checkbox"
-                      checked={selectedRole === "investor"}
+                      className="userlogin-radio"
+                      checked={selectedRole === "investee"}
                       onChange={handleRadioChange}
                     />
-                    <h5>Investee</h5>
+                    <h5 className="userlogin-inputfields-label">Investee</h5>
                   </div>
                 </div>
 
                 <div className="userlogin-loginarea-left-loginform-inputarea">
-                  <h5>Email</h5>
+                  <h5 className="userlogin-inputfields-label">Email</h5>
                   <input
                     type="email"
                     className=" userlogin-loginarea-left-loginform-inputarea-inputboxes"
                   />
                 </div>
                 <div className="userlogin-loginarea-left-loginform-inputarea">
-                  <h5>Password</h5>
+                  <h5 className="userlogin-inputfields-label">Password</h5>
                   <input
                     type="password"
                     className="userlogin-loginarea-left-loginform-inputarea-inputboxes"
@@ -71,13 +77,33 @@ const Userlogin = () => {
                 <div className="userlogin-loginarea-left-loginform-submitarea flex align-center justify-center">
                   <input
                     type="submit"
-                    value="Login"
+                    value="Sign In"
                     className="userlogin-loginform-submitbutton"
                   />
                 </div>
               </form>
+              <div className="userlogin-dropdownbutton">
+
+              </div>
             </div>
-            <div className="userlogin-loginarea-right"></div>
+            <div className="userlogin-loginarea-right flex flex-column align-center justify-center">
+              <div className="userlogin-loginarea-right-content flex flex-column align-center justify-center">
+                <h1 className="userlogin-loginarea-right-content-heading">
+                  New Here?
+                </h1>
+                <h3 className="userlogin-loginarea-right-content-text">
+                  Sign Up and unlock the potential of your financial future –
+                  Join our business investment platform today and start building
+                  wealth!
+                </h3>
+                <button
+                  className="userlogin-loginarea-right-signupbutton"
+                  onClick={() => navigate("/user-registeration/user-role")}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const mongo = require('mongoose')
+const env = require('dotenv').config()
+const cors = require('cors')
+const PORT = process.env.PORT || 3001
+const verifyToken = require('./middleware/verifyToken')
+const AuthRouter = require('./controller/Auth')
+
+mongo.connect(process.env.MONGO_URL)
+.then(res => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDb Connection Failed' , err))
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+app.use(cors())
+
+
+app.get('/' ,verifyToken , function(req , res){
+    res.json({ user : req.user })
+})
+
+app.use('/auth' , AuthRouter)
+
+
+
+app.listen(PORT , ()=> console.log(`Listening on http://localhost:${PORT}`))
