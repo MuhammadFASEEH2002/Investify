994c064ad5a4a1b1f33a60efe9d6828e075faf46
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2023-10-12 20:52:51 +0500

    made changes in Userrole.css

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const mongo = require('mongoose')
 const env = require('dotenv').config()
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
-const verifyToken = require('./middleware/verifyToken')
-const AuthRouter = require('./controller/Auth')
+// const verifyToken = require('./middleware/verifyToken')
+// const AuthRouter = require('./controller/Auth')
 
 mongo.connect(process.env.MONGO_URL)
 .then(res => console.log('MongoDB connected'))
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended : true }))
 app.use(cors())
 
 
-app.get('/' ,verifyToken , function(req , res){
-    res.json({ user : req.user })
-})
+// app.get('/' ,verifyToken , function(req , res){
+//     res.json({ user : req.user })
+// })
 
-app.use('/auth' , AuthRouter)
+// app.use('/auth' , AuthRouter)
 
 
 
