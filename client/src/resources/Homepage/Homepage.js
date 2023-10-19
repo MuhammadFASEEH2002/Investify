03fcaf7a40101de19fcdba51b288f35a67441cd0
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2023-10-19 17:59:56 +0500

    Homepage made using Chakra

--- client/src/resources/Homepage/Homepage.js
+++ client/src/resources/Homepage/Homepage.js
@@ -105,39 +105,35 @@ const Homepage = () => {
             zIndex={3}
           >
             <Stack
-           
-              width={{base:"20%", md:"30%", lg:"50%"}}
-
+              width={{ base: "20%", md: "30%", lg: "50%" }}
               alignItems={"center"}
               justifyContent={"start"}
               flexDirection={"row"}
             >
               <Stack
-                height={{base:"30px", md:"60px", lg:"60px"}}
-                width={{base:"30px", md:"50px", lg:"50px"}}
+                height={{ base: "30px", md: "60px", lg: "60px" }}
+                width={{ base: "30px", md: "50px", lg: "50px" }}
                 backgroundImage="url('/images/logo.png')"
                 backgroundSize={"100% 100%"}
               ></Stack>
             </Stack>
             <Stack
-
-              width={{base:"80%", md:"70%", lg:"50%"}}
-
+              width={{ base: "80%", md: "70%", lg: "50%" }}
               alignItems={"center"}
               justifyContent={"center"}
             >
               <UnorderedList
                 style={{ display: "flex", flexDirection: "row" }}
-                width={"60%"}
+                width={"100%"}
                 alignItems={"center"}
                 justifyContent={"center"}
               >
                 {navigationButtons.map((item) => (
                   <ListItem
                     key={item.id}
-              marginLeft={{base:"10px", md:"15px", lg:"25px"}}
-              marginRight={{base:"10px", md:"15px", lg:"25px"}}
-                  fontSize={{base:"0.8rem",md:"1rem", lg:"1rem"}}
+                    marginLeft={{ base: "10px", md: "15px", lg: "25px" }}
+                    marginRight={{ base: "10px", md: "15px", lg: "25px" }}
+                    fontSize={{ base: "0.7rem", md: "1rem", lg: "1rem" }}
                     listStyleType={"none"}
                   >
                     <Link
@@ -152,8 +148,14 @@ const Homepage = () => {
               </UnorderedList>
             </Stack>
             <Stack alignItems={"flex-start"}>
-              <Button colorScheme="gray" rightIcon={<ArrowForwardIcon />} marginRight={"10px"}>
-                Get Started
+              <Button
+                colorScheme="gray"
+                rightIcon={<ArrowForwardIcon />}
+                marginRight={"10px"}
+                size={{ base: "xs", md: "sm", lg: "md" }}
+                onClick={() => navigate("/user-login")}
+              >
+                Explore
               </Button>
             </Stack>
           </HStack>
@@ -163,6 +165,79 @@ const Homepage = () => {
           <HomepageAboutus />
           {/* Services section */}
           <HomepageServices />
+          <HStack
+            width={"100%"}
+            bgGradient="linear(to-r, #42B2FE, #001F44)"
+            padding={"20px 15px 20px 15px"}
+            flexDirection={{ base: "column", md: "row", lg: "row" }}
+          >
+            <Stack
+              width={"30%"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <Stack
+                width={{ base: "80px", md: "100px", lg: "150px" }}
+                height={{ base: "80px", md: "100px", lg: "150px" }}
+                backgroundSize={"100% 100%"}
+                backgroundImage="url('/images/logo.png')"
+              ></Stack>
+            </Stack>
+            <Stack
+              width={"40%"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <Heading size={"lg"} color={"white"}>
+                Investify
+              </Heading>
+              <UnorderedList
+                style={{ display: "flex", flexDirection: "column" }}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                {navigationButtons.map((item) => (
+                  <ListItem
+                    key={item.id}
+                    fontSize={{ base: "0.7rem", md: "1rem", lg: "1rem" }}
+                    listStyleType={"none"}
+                  >
+                    <Link
+                      href={item.link}
+                      textDecoration={"none"}
+                      color={"white"}
+                    >
+                      {item.title}
+                    </Link>
+                  </ListItem>
+                ))}
+              </UnorderedList>
+            </Stack>
+            <HStack
+              width={"30%"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <Stack
+                width={{ base: "20px", md: "30px", lg: "40px" }}
+                height={{ base: "20px", md: "30px", lg: "40px" }}
+                backgroundSize={"100% 100%"}
+                backgroundImage="url('/images/whatsapp.png')"
+              ></Stack>
+              <Stack
+                width={{ base: "20px", md: "30px", lg: "40px" }}
+                height={{ base: "20px", md: "30px", lg: "40px" }}
+                backgroundSize={"100% 100%"}
+                backgroundImage="url('/images/fb.png')"
+              ></Stack>
+              <Stack
+                width={{ base: "20px", md: "30px", lg: "40px" }}
+                height={{ base: "20px", md: "30px", lg: "40px" }}
+                backgroundSize={"100% 100%"}
+                backgroundImage="url('/images/insta.png')"
+              ></Stack>
+            </HStack>
+          </HStack>
         </Stack>
       </Stack>
     </>
