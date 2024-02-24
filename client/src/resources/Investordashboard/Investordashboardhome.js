d703ee3e5fb9b763ee2be2d0a27b2d49298f7604
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-24 19:25:14 +0500

    update

--- client/src/resources/Investordashboard/Investordashboardhome.js
+++ client/src/resources/Investordashboard/Investordashboardhome.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from './components/Sidebar'
+import { useNavigate } from 'react-router-dom';
 
 
 const Investordashboardhome = () => {
+  const navigate=useNavigate();
+  useEffect(() => {
+    if (window.localStorage.getItem('token1')) {
+      document.title = "Investify | Investor-Home";
+    } else {
+      navigate("/user-login");
+    }
+  }, []);
   return (
     <>
   
