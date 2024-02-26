3d92cc62ea0dbc586d312724dd3cf5a493ea3bd9
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-26 07:08:36 +0500

    Update Investeedashboardhome.js

--- client/src/resources/Investeedashboard/Investeedashboardhome.js
+++ client/src/resources/Investeedashboard/Investeedashboardhome.js
@@ -36,7 +36,7 @@ const Investeedashboardhome = () => {
   const getUser = () => {
     setLoading(true)
     const token = window.localStorage.getItem('token');
-    fetch("http://127.0.0.1:3001/api/investee/get-user", {
+    fetch(`${process.env.REACT_APP_FETCH_URL_}/api/investee/get-user`, {
       method: "GET",
       headers: {
         'token': token,
