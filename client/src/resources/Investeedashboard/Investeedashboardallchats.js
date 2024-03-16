d70b56f6451fdc30a6f26064d8a7ce9e9a1e882e
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-03-16 18:27:46 +0500

    chat 80%

--- client/src/resources/Investeedashboard/Investeedashboardallchats.js
+++ client/src/resources/Investeedashboard/Investeedashboardallchats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from './components/Sidebar';
-import { Button } from '@chakra-ui/react';
+import { Button, Stack, Spinner } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { db } from '../../utils/firebase';
 import {
@@ -18,6 +18,7 @@ import useInvestee from '../../providers/investeeStore';
 const Investeedashboardallchats = () => {
     const messagesRef = collection(db, 'messages');
     const [roomIdsArray, setRoomIdsArray] = useState([]);
+    const [loading, setLoading] = useState(false);
     const investee = useInvestee((state) => state?.investees);
     const navigate = useNavigate();
 
@@ -31,18 +32,18 @@ const Investeedashboardallchats = () => {
                     const distinctRoomIds = new Set();
                     snapshot.forEach((doc) => {
                         const { roomId } = doc.data();
-                        console.log(roomId.split('_'))
-                        if(roomId.split('_')[0]==investee?._id || roomId.split('_')[1]==investee?._id){
+
+                        if (roomId.split('_')[0] == investee?._id || roomId.split('_')[1] == investee?._id) {
                             distinctRoomIds.add(roomId);
-                        }else{
+                        } else {
                             console.log('not same')
                         }
-                        
+
                     });
                     const roomIds = Array.from(distinctRoomIds);
                     setRoomIdsArray(roomIds);
                 });
-    
+
                 return () => {
                     unsubscribe();
                 };
@@ -57,24 +58,29 @@ const Investeedashboardallchats = () => {
 
     return (
         <Sidebar>
-            {roomIdsArray.map((roomId) => (
-                <>
-                    <Button
-                        key={roomId}
-                        colorScheme="blue"
-                        variant="outline"
-                        onClick={() => {
-                            navigate(`/user/investee-dashboard/chat/${roomId.split('_')[0]}/${roomId.split('_')[1]}`);
-                        }}
-                    >
-                        {roomId}
-                    </Button>
-                </>
-            ))}
-            <p>
-
-                testing zustand :{investee?._id}
-            </p>
+            {loading ? (<>
+                <><Stack minHeight={'100%'} width={'100%'} alignItems={"center"} justifyContent={"center"} ><Spinner size='xl' /></Stack> </>
+            </>) : (<>
+                {roomIdsArray.length > 0 ? (<>
+                    {roomIdsArray.map((roomId,index) => (
+                        <>
+                            <Button
+                                key={roomId}
+                                colorScheme="blue"
+                                variant="outline"
+                                onClick={() => {
+                                    navigate(`/user/investee-dashboard/chat/${roomId.split('_')[0]}/${roomId.split('_')[1]}`);
+                                }}
+                            >
+                                chat {index+1}
+                            </Button>
+                            <p>
+                                {/* Testing Zustand: {investee?._id} */}
+                            </p>
+                        </>
+                    ))}
+                </>) : (<>No Chats</>)}
+            </>)}
         </Sidebar>
     );
 };
--- client/src/resources/Investordashboard/Investordashboardallchats.js
+++ client/src/resources/Investordashboard/Investordashboardallchats.js
@@ -19,26 +19,30 @@ const Investordashboardallchats = () => {
   const [roomIdsArray, setRoomIdsArray] = useState([]);
   const [loading, setLoading] = useState(false);
   const investor = useInvestor((state) => state?.investors)
-const navigate=useNavigate()
-  
+  const navigate = useNavigate()
+
 
   useEffect(() => {
     if (window.localStorage.getItem('token1')) {
       document.title = 'Investify | Investor-All-Chats';
-  
+
       const queryMessages = query(messagesRef, orderBy('roomId'));
-  
+
       setLoading(true);
       const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-  
+
         const distinctRoomIds = new Set();
         snapshot.forEach((doc) => {
           const { roomId } = doc.data();
-          distinctRoomIds.add(roomId);
+          if (roomId.split('_')[0] == investor?._id || roomId.split('_')[1] == investor?._id) {
+            distinctRoomIds.add(roomId);
+          } else {
+            console.log('not same')
+          }
+
         });
         const roomIds = Array.from(distinctRoomIds);
         setRoomIdsArray(roomIds);
-  
         setLoading(false);
       });
       console.log(investor);
@@ -56,23 +60,26 @@ const navigate=useNavigate()
       {loading ? (<>
         <><Stack minHeight={'100%'} width={'100%'} alignItems={"center"} justifyContent={"center"} ><Spinner size='xl' /></Stack> </>
       </>) : (<>
-        {roomIdsArray.map((roomId) => (
-          <>
-          <Button
-            key={roomId}
-            colorScheme="blue"
-            variant="outline"
-            onClick={() => {
-              navigate(`/user/investor-dashboard/chat/${roomId.split('_')[0]}/${roomId.split('_')[1]}`);
-            }}
-          >
-            {roomId}
-          </Button>
-          <p>
-            Testing Zustand: {investor._id}
-          </p>
-          </>
-        ))}
+        {roomIdsArray.length > 0 ? (<>
+          {roomIdsArray.map((roomId, index) => (
+            <>
+              <Button
+                key={roomId}
+                colorScheme="blue"
+                variant="outline"
+                onClick={() => {
+                  navigate(`/user/investor-dashboard/chat/${roomId.split('_')[0]}/${roomId.split('_')[1]}`);
+                }}
+              >
+                chat {index + 1}
+              </Button>
+              <p>
+                {/* Testing Zustand: {investor._id} */}
+              </p>
+            </>
+          ))}
+        </>) : (<>No Chats</>)}
+
 
       </>)}
     </Sidebar>
