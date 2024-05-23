f0134157006f2e9d1067a60d01e882cd19fd88d3
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-05-23 19:45:08 +0500

    working on profit returns

--- client/src/resources/Admindashboard/Admindashboardinvestments.js
+++ client/src/resources/Admindashboard/Admindashboardinvestments.js
@@ -67,7 +67,7 @@ const Admindashboardinvestments = () => {
             <>
               <JTable
                 tableData={investment}
-                tableHeads={['Business Name', 'Amount Given', 'Investment Duration', 'Profit Share Percentage', 'Investment Start Date', 'Investment End Date', 'Investment Status']}
+                tableHeads={['Business Name', 'Amount Given', 'Investment Duration', 'Profit Share Percentage', 'Investment Start Date', 'Investment End Date', 'Investment Status', 'Pay Profits']}
 
                 tableRender={(index, investment) => {
                   return <Row key={index} investment={investment} />;
@@ -88,129 +88,109 @@ const Admindashboardinvestments = () => {
 
 const Row = ({ investment }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const handleInputChange = (event, setState) => {
+    setState(event.target.value);
+  };
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const [totalProfit, setTotalProfit] = useState('');
+  const [profitToGive, setProfitToGive] = useState('');
 
+  const handleProfitChange = (event) => {
+    const profit = event.target.value;
+    setTotalProfit(profit);
+
+    const calculatedProfit = profit ? (parseFloat(profit) * 0.05).toFixed(2) : '';
+    setProfitToGive(calculatedProfit);
+  };
   const onOpen = () => {
     setIsModalOpen(true);
   };
 
   const onClose = () => {
     setIsModalOpen(false);
-    // setOtp(false);
-    // setForgotEmailDisable(false);
   };
-  return(
-  <>
-    <Tr
-      _hover={{
-        backgroundColor: 'gray.100',
-        cursor: 'pointer'
-      }}
-    >
-      <Td style={{ fontWeight: 'bold' }} color={"blue"}><Link to={`/admin/admin-dashboard/investment-detail/${investment?._id}`} >{investment?.investee_id?.businessName}</Link></Td>
-      <Td>Rs {investment?.amount} /- </Td>
-      <Td>{investment?.investmentDuration} years </Td>
-      <Td>{investment?.profitPercentage} % </Td>
-      <Td>{investment?.investment_start_date} </Td>
-      <Td>{investment?.investment_end_date}  </Td>
+  return (
+    <>
+      <Tr
+        _hover={{
+          backgroundColor: 'gray.100',
+          cursor: 'pointer'
+        }}
+      >
+        <Td style={{ fontWeight: 'bold' }} color={"blue"}><Link to={`/admin/admin-dashboard/investment-detail/${investment?._id}`} >{investment?.investee_id?.businessName}</Link></Td>
+        <Td>Rs {investment?.amount} /- </Td>
+        <Td>{investment?.investmentDuration} years </Td>
+        <Td>{investment?.profitPercentage} % </Td>
+        <Td>{investment?.investment_start_date} </Td>
+        <Td>{investment?.investment_end_date}  </Td>
 
-      <Td>{investment?.isInvestmentEnded ? 'Ended' : 'Not Ended'}</Td>
-      {investment?.isInvestmentEnded && (
-        <Td>
-          <Button onClick={() => onOpen()} colorScheme="teal" variant="solid">
-            pay profits
-          </Button>
-        </Td>
-      )}
+        <Td>{investment?.isInvestmentEnded ? 'Ended' : 'Not Ended'}</Td>
+        {investment?.isInvestmentEnded && (
+          <Td>
+            <Button onClick={() => onOpen()} colorScheme="teal" variant="solid">
+              pay profits
+            </Button>
+          </Td>
+        )}
 
 
-    </Tr>
-    <Modal onClose={onClose} isOpen={isModalOpen} isCentered>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader></ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
+      </Tr>
+      <Modal onClose={onClose} isOpen={isModalOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
 
-          <VStack alignItems={"flex-start"}>
+            <VStack alignItems={"flex-start"}>
+              <Text>Enter Total Profits from {investment?.investment_start_date} till {investment?.investment_end_date}</Text>
+              <HStack>
+                <Input
+                  type="number"
+                  placeholder="Enter Total Profits Here"
+                  variant={"filled"}
+                  border={"0.5px solid grey"}
+                  value={totalProfit}
+                  onChange={handleProfitChange}
+                />
 
-            <Text>Select Role:</Text>
+              </HStack>
+              <Text>Profit to be Given</Text>
+              <HStack>
+                <Input
+                  type="text"
+                  variant={"filled"}
+                  border={"0.5px solid grey"}
+                  readOnly={true}
+                  value={profitToGive}
+                />
+                {/* <Button onClick={() => sendOtp()} colorScheme="teal" variant="solid">Get OTP</Button> */}
 
-            <Text>Email address</Text>
-            <HStack>
+              </HStack>
+              <Text>Upload Profits Account Statement (signed by auditor) </Text>
               <Input
-                type="email"
-                placeholder="Enter Your email"
-                width={{ base: "100%", md: "100%", lg: "80%" }}
-                variant={"filled"}
-                border={"0.5px solid grey"}
-              // readOnly={forgotEmailDisable}
-              // onChange={(event) => handleInputChange(event, setForgotEmail)}
+                size="md"
+                type="file"
+                width={"90%"}
+                accept=".pdf"
+              // onChange={handleFileChange}
               />
-              {/* <Button onClick={() => sendOtp()} colorScheme="teal" variant="solid">Get OTP</Button> */}
-
-            </HStack>
-
-
-
-            <Text>Enter OTP</Text>
-            <Input
-              type="number"
-              placeholder="Enter 4 digit OTP."
-              width={{ base: "100%", md: "100%", lg: "80%" }}
-              variant={"filled"}
-              border={"0.5px solid grey"}
-            // onChange={(event) => handleInputChange(event, setOtpNumber)}
-            // onKeyDown={(event) => checkIfNumber(event)}
-            />
-            <Text>New Password</Text>
-            <InputGroup width={{ base: "100%", md: "100%", lg: "80%" }}>
-              <Input
-                pr="4.5rem"
-                // type={showNewPass ? "text" : "password"}
-                placeholder="Enter password"
-                variant={"filled"}
-                border={"0.5px solid grey"}
-              // onChange={(event) =>
-              //   handleInputChange(event, setNewPassword)
-              // }
-              />
-              <InputRightElement width="4.5rem">
-                {/* <Button h="1.75rem" size="sm" onClick={handleNewPassClick}>
-                              {showNewPass ? "Hide" : "Show"}
-                            </Button> */}
-              </InputRightElement>
-            </InputGroup>
-            <Text>Confirm New Password</Text>
-            <InputGroup width={{ base: "100%", md: "100%", lg: "80%" }}>
-              <Input
-                pr="4.5rem"
-                // type={showConfirmPass ? "text" : "password"}
-                placeholder="Enter password"
-                variant={"filled"}
-                border={"0.5px solid grey"}
-              // onChange={(event) =>
-              //   handleInputChange(event, setConfirmPassword)
-              // }
-              />
-              <InputRightElement width="4.5rem">
-                {/* <Button h="1.75rem" size="sm" onClick={handleConfirmPassClick}>
-                              {showConfirmPass ? "Hide" : "Show"}
-                            </Button> */}
-              </InputRightElement>
-            </InputGroup>
-          </VStack>
-        </ModalBody>
-        <ModalFooter>
-          {/* {otp ? (
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            {/* {otp ? (
                       <Stack>
                         <Button colorScheme="teal" variant="solid" onClick={() => { updatePassword() }}>Update Password</Button>
                       </Stack>
                     ) : (<></>)} */}
 
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  </>)
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>)
 }
 
 
