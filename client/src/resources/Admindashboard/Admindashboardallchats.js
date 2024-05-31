ef7a05e1b83aa2444c6ed75d739101b961747b24
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-06-01 01:05:22 +0500

    upgraded admin chat UI

--- client/src/resources/Admindashboard/Admindashboardallchats.js
+++ client/src/resources/Admindashboard/Admindashboardallchats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from './components/Sidebar';
-import { Stack, Spinner, Card, CardBody, Text, Heading } from '@chakra-ui/react';
+import { Stack, Spinner, Card, CardBody, Text, Heading,Flex,Box,Spacer } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import useInvestee from '../../providers/investeeStore';
 
@@ -46,8 +46,9 @@ const Admindashboardallchats = () => {
         }
     }
     const getChats = (messages) => {
-        const distinctChats = new Set();
-        messages.map((message) => {
+        const distinctChats = new Map();
+
+        messages.forEach((message) => {
             //    console.log(message)
             if (message.chat_id.split('_')[0] === '65d88f93e5d99c47ee8df0dd' || message.chat_id.split('_')[1] === '65d88f93e5d99c47ee8df0dd') {
                 let chatId = message.chat_id;
@@ -63,16 +64,18 @@ const Admindashboardallchats = () => {
               
                 if (chatName) {
                   let uniqueIdentifier = `${chatId}-${chatName}`;
-                  distinctChats.add(uniqueIdentifier);
+                let chatTime = message?.createdAt;
+
+                if (!distinctChats.has(uniqueIdentifier) || new Date(chatTime) > new Date(distinctChats.get(uniqueIdentifier).createdAt)) {
+                    distinctChats.set(uniqueIdentifier, message);
+                }
                 }
               }
 
         });
         // console.log(distinctChats)
-        const chatHeads = Array.from(distinctChats);
-                // console.log(chatHeads);
-                setChats(chatHeads);
-                // console.log(chats)
+        const chatHeads = Array.from(distinctChats.values());
+        setChats(chatHeads);
     }
     useEffect(() => {
         if (window.localStorage.getItem('adminToken')) {
@@ -87,31 +90,119 @@ const Admindashboardallchats = () => {
 
 
     return (
+        // <Sidebar>
+        //     {loading ? (<>
+        //         <><Stack minHeight={'100%'} width={'100%'} alignItems={"center"} justifyContent={"center"} ><Spinner size='xl' /></Stack> </>
+        //     </>) : (<>
+        //         {chats.length > 0 ? 
+        //         (
+        //             <>
+        //                 <Stack width={"100%"} alignItems={"center"} justifyContent={"center"}>
+        //             <Heading>Customer Support Chats</Heading>
+        //                     <Stack width={{ base: "100%", md: "80%", lg: "70%" }} flexDirection={"column"}>
+        //                         {/* {chats.map((chat,index) => (
+        //                             <Card onClick={() => {
+        //                                 navigate(`/admin/admin-dashboard/chat-support/${chat?.split('-')[0].split('_')[0]}/${chat?.split('-')[0].split('_')[1]}`);
+        //                             }} cursor={"pointer"} key={index} width={"100%"}>
+        //                                 <CardBody>
+        //                                     <Text fontSize={"1em"}>{chat.split('-')[1]} </Text>
+        //                                 </CardBody>
+        //                             </Card>
+        //                         ))} */}
+        //                                {chats.map((chat, index) => (
+        //                                 <Card
+        //                                     onClick={() => {
+        //                                         const chatIdParts = chat.chat_id.split('_');
+        //                                         navigate(`/admin/admin-dashboard/chat-support/${chatIdParts[0]}/${chatIdParts[1]}`);
+        //                                     }}
+        //                                     cursor="pointer"
+        //                                     key={index}
+        //                                     width="100%"
+        //                                     mb={4}
+        //                                     boxShadow="md"
+        //                                     borderRadius="md"
+        //                                     _hover={{ boxShadow: "lg", transform: "scale(1.02)", transition: "all 0.2s" }}
+        //                                 >
+        //                                     <CardBody>
+        //                                         <Flex alignItems="center">
+        //                                             <Box>
+        //                                                 <Text fontSize="1.2em" fontWeight="bold">
+        //                                                 {chat.investee_id?.businessName || `${chat.investor_id?.firstName} ${chat.investor_id?.lastName}`}
+
+        //                                                 </Text>
+        //                                                 {/* <Text color="gray.500" fontSize="0.9em">
+        //                                                     {chat.investor_id.businessName || "No Business Name"}
+        //                                                 </Text> */}
+        //                                             </Box>
+        //                                             <Spacer />
+        //                                             <Box textAlign="right">
+        //                                                 <Text color="gray.400" fontSize="0.8em">
+        //                                                     {new Date(chat.createdAt).toLocaleString()}
+        //                                                 </Text>
+        //                                             </Box>
+        //                                         </Flex>
+        //                                     </CardBody>
+        //                                 </Card>
+        //                             ))}
+        //                     </Stack>
+        //                 </Stack>
+        //             </>
+        //         ) : (<>No Chats</>)}
+        //     </>)}
+        // </Sidebar>
         <Sidebar>
-            {loading ? (<>
-                <><Stack minHeight={'100%'} width={'100%'} alignItems={"center"} justifyContent={"center"} ><Spinner size='xl' /></Stack> </>
-            </>) : (<>
-                {chats.length > 0 ? 
-                (
-                    <>
-                        <Stack width={"100%"} alignItems={"center"} justifyContent={"center"}>
-                    <Heading>Customer Support Chats</Heading>
-                            <Stack width={{ base: "100%", md: "80%", lg: "70%" }} flexDirection={"column"}>
-                                {chats.map((chat,index) => (
-                                    <Card onClick={() => {
-                                        navigate(`/admin/admin-dashboard/chat-support/${chat?.split('-')[0].split('_')[0]}/${chat?.split('-')[0].split('_')[1]}`);
-                                    }} cursor={"pointer"} key={index} width={"100%"}>
-                                        <CardBody>
-                                            <Text fontSize={"1em"}>{chat.split('-')[1]} </Text>
-                                        </CardBody>
-                                    </Card>
-                                ))}
-                            </Stack>
+    {loading ? (
+        <>
+            <Stack minHeight={'100%'} width={'100%'} alignItems={"center"} justifyContent={"center"} ><Spinner size='xl' /></Stack>
+        </>
+    ) : (
+        <>
+            {chats.length > 0 ? (
+                <>
+                    <Stack width={"100%"} alignItems={"center"} justifyContent={"center"}>
+                        <Heading>Customer Support Chats</Heading>
+                        <Stack width={{ base: "100%", md: "80%", lg: "70%" }} flexDirection={"column"}>
+                            {chats.map((chat, index) => (
+                                <Card
+                                    onClick={() => {
+                                        const chatIdParts = chat.chat_id.split('_');
+                                        navigate(`/admin/admin-dashboard/chat-support/${chatIdParts[0]}/${chatIdParts[1]}`);
+                                    }}
+                                    cursor="pointer"
+                                    key={index}
+                                    width="100%"
+                                    mb={4}
+                                    boxShadow="md"
+                                    borderRadius="md"
+                                    _hover={{ boxShadow: "lg", transform: "scale(1.02)", transition: "all 0.2s" }}
+                                >
+                                    <CardBody>
+                                        <Flex alignItems="center" flexDirection={{ base: "column", md: "row" }}>
+                                            <Box mb={{ base: 2, md: 0 }}>
+                                                <Text fontSize="1.2em" fontWeight="bold">
+                                                    {chat.investee_id?.businessName || `${chat.investor_id?.firstName} ${chat.investor_id?.lastName}`}
+                                                </Text>
+                                            </Box>
+                                            <Spacer />
+                                            <Box textAlign={{ base: "center", md: "right" }} mt={{ base: 2, md: 0 }}>
+                                                <Text color="gray.400" fontSize="0.8em">
+                                                    {new Date(chat.createdAt).toLocaleString()}
+                                                </Text>
+                                            </Box>
+                                        </Flex>
+                                    </CardBody>
+                                </Card>
+                            ))}
                         </Stack>
-                    </>
-                ) : (<>No Chats</>)}
-            </>)}
-        </Sidebar>
+                    </Stack>
+                </>
+            ) : (
+                <>No Chats</>
+            )}
+        </>
+    )}
+</Sidebar>
+
     );
 };
 
